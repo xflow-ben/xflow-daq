5bea621bbd6234731c5815d990b37aa0e84de1d5
xflow-ben <ben@xflowenergy.com>
2024-09-10 12:43:31 -0600

    adding some daq test scripts

--- acquisition/implementations/counterInTest.m
+++ acquisition/implementations/counterInTest.m
@@ -13,8 +13,8 @@ d.logging.fileNamePrefix = 'anno_and_RPM_TEST';
 d.acquisitionType = 'finite';
 d.logging.mode = 'log';
 
-d.logging.directoryPath = 'C:\Users\Ben Strom\Documents\Loads_Data\load_calibrations';
-d.durationSeconds = 20;
+d.logging.directoryPath = 'C:\Users\Ben Strom\Documents\Loads_Data\';
+d.durationSeconds = 60;
 
 
 %% 9237 Bridge modules (strain channels)
@@ -36,11 +36,11 @@ d.task(3).sampleClock.source = 'userDefined';
 d.task(3).sampleClock.terminal = '/cDAQ9184-1A4BA5D/ai/SampleClock';
 d.task(3).setCICICountEdgesTerm('anno2','PFI5') % NE Anno
 
-d.createTask('CI','RPM',512,3);
-d.task(4).addChannel('CICountEdges', 'cDAQ9184-1A4BA5DMod1', 'ctr1', 'RPM', 'rising',0,'up');
-d.task(4).sampleClock.source = 'userDefined';
-d.task(4).sampleClock.terminal = '/cDAQ9184-1A4BA5D/ai/SampleClock';
-d.task(4).setCICICountEdgesTerm('RPM','PFI3')
+% d.createTask('CI','RPM',512,3);
+% d.task(4).addChannel('CICountEdges', 'cDAQ9184-1A4BA5DMod1', 'ctr1', 'RPM', 'rising',0,'up');
+% d.task(4).sampleClock.source = 'userDefined';
+% d.task(4).sampleClock.terminal = '/cDAQ9184-1A4BA5D/ai/SampleClock';
+% d.task(4).setCICICountEdgesTerm('RPM','PFI3')
 
 % % adding digital filter - needed for debounce
 % d.task(2).enableCICountEdgesFltr('anno1');
@@ -55,10 +55,10 @@ d.task(4).setCICICountEdgesTerm('RPM','PFI3')
 % d.task(3).enableCICountEdgesFltr('anno2');
 % d.task(3).setCICountEdgesDigFltrMinPulseWidth('anno2',6.4e-7)
 d.task(2).enableCICountEdgesFltr('anno1');
-d.task(2).setCICountEdgesDigFltrMinPulseWidth('anno1',20.0e-6)
+d.task(2).setCICountEdgesDigFltrMinPulseWidth('anno1',6.4e-6)
 d.task(2).setCICountEdgesDigFltrTimebaseSrc('anno1','/cDAQ9184-1A4BA5D/100kHzTimebase')
 d.task(3).enableCICountEdgesFltr('anno2');
-d.task(3).setCICountEdgesDigFltrMinPulseWidth('anno2',20.0e-6)
+d.task(3).setCICountEdgesDigFltrMinPulseWidth('anno2',6.4e-6)
 d.task(3).setCICountEdgesDigFltrTimebaseSrc('anno2','/cDAQ9184-1A4BA5D/100kHzTimebase')
 
 % d.createTask('CI','encInput',512,4);
@@ -70,7 +70,7 @@ d.task(3).setCICountEdgesDigFltrTimebaseSrc('anno2','/cDAQ9184-1A4BA5D/100kHzTim
 d.configureLogging();
 d.task(2).configSampleClock();
 d.task(3).configSampleClock();
-d.task(4).configSampleClock();
+% d.task(4).configSampleClock();
 d.task(1).configSampleClock();
 
 % d.task(5).configSampleClock();
