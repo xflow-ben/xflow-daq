a363628e50a8cbc58ad59754313813b858094e9d
xflow-ian <68661656+xflow-ian@users.noreply.github.com>
2024-09-11 13:55:02 -0700

    Added capability to save statistical data (sd) and not save time data (td)

--- processing/implementations/10m_Spanish_Fork_testing/guy_wire_tensioning.m
+++ processing/implementations/10m_Spanish_Fork_testing/guy_wire_tensioning.m
@@ -6,31 +6,36 @@ clc
 files.absolute_data_dir = 'X:\Experiments and Data\20 kW Prototype\Loads_Data';
 files.relative_experiment_dir = 'GW_tensioning'; % This is relative to files.absolute_data_dir
 files.relative_tare_dir= 'tares'; % This is relative to files.absolute_data_dir
-files.data_name_conventions = {'guy_wire_cal_towerBaseStrain_*.tdms'};
+
 
 %% Load calibration struct
 load('C:\Users\Ian\Documents\GitHub\xflow-daq\processing\implementations\10m_Spanish_Fork_testing\Calibrations\Results\cal_struct_05_09_24.mat')
 
 %% Load constants
 consts = XFlow_Spanish_Fork_testing_constants();
+% Put any values you want to override here
+% consts.data.data_name_conventions = {'guy_wire_cal_towerBaseStrain_*.tdms'};
+consts.data.save_types = {'sd'};
+consts.data.N = 1; % sd averaging time [s]
 
 %% Process data folder
 results = process_data_folder(files,cal,consts);
 
 %% Plot
-fields = fieldnames(results.td);
-
-% only plot most recent tensioning attempt
 figure
-count = 0;
-II = length(results.td);
-for JJ = 1:length(fields)
-    if ~sum(strcmp(fields{JJ},'time'))
-        count = count + 1;
-        plot(results.td(II).(fields{JJ})/consts.units.lbf_to_N)
-        hold on
-        legStr{count} = fields{JJ};
+for II = length(results)
+    fields = fieldnames(results(II).sd);
+    % only plot most recent tensioning attempt
+    count = 0;
+    JJ = length(results(II).sd);
+    for KK = 1:length(fields)
+        if ~sum(strcmp(fields{KK},'time'))
+            count = count + 1;
+            plot(results(II).sd(JJ).(fields{KK})/consts.units.lbf_to_N,'o')
+            hold on
+            legStr{count} = fields{KK};
+        end
     end
+    legend(legStr,'Location','best','Interpreter','none')
+    ylabel('Tension [lbf]')
 end
-legend(legStr,'Location','best','Interpreter','none')
-ylabel('Tension [lbf]')
--- processing/utilities/Data Analysis/process_data_folder.m
+++ processing/utilities/Data Analysis/process_data_folder.m
@@ -3,11 +3,6 @@ function results = process_data_folder(files,cal,consts)
 % cal is an array of calibration structs
 % consts is a struct with project specific values
 
-% Initialize the data name conventions which will be processed
-if sum(strcmp(fieldnames(files),'data_name_conventions')) == 0
-    files.data_name_conventions = consts.data.data_file_name_conventions;
-end
-
 %% Load tares
 % Check if list of applicable tares is present in dataDir
 tareList = dir(fullfile(files.absolute_data_dir,files.relative_experiment_dir,'tare*.mat'));
@@ -24,15 +19,15 @@ end
 % Create tare struct
 % tare is a struct array with XX elements where XX specifies the naming
 % convention for the files it applies to. Each tare(XX) contains:
-% data_name_conventions: the naming convention for the files this tare 
+% data_name_conventions: the naming convention for the files this tare
 % struct applies to
 % chanNames: The data channel names
 % data: The median values from each tare file fom tareList which matches
 % the naming convention
 
-for II = 1:length(files.data_name_conventions)
+for II = 1:length(consts.data.file_name_conventions)
     count = 0;
-    pattern = strrep(files.data_name_conventions{II}, '*', '.*');
+    pattern = strrep(consts.data.file_name_conventions{II}, '*', '.*');
     for JJ = 1:length(tareList)
         if ~isempty(regexp(tareList{JJ},pattern,'once'))
             count = count + 1;
@@ -40,7 +35,7 @@ for II = 1:length(files.data_name_conventions)
             tare_td = convertTDMStoXFlowFormat(tare_TDMS);
             tare(II).data(count,:) = median(tare_td.data,1);
             if count == 1
-                tare(II).data_name_conventions = files.data_name_conventions{II};
+                tare(II).data_name_conventions = consts.data.file_name_conventions{II};
                 tare(II).chanNames = tare_td.chanNames;
             end
         end
@@ -52,57 +47,63 @@ end
 % Loop through all the data files and run them through process_data_point
 
 % Find file names that start with the specified naming convensions
-for II = 1:length(files.data_name_conventions)
-    II/length(files.data_name_conventions)
-    dataFiles = dir(fullfile(files.absolute_data_dir,files.relative_experiment_dir,files.data_name_conventions{II}));
+for II = 1:length(consts.data.file_name_conventions)
+    dataFiles = dir(fullfile(files.absolute_data_dir,files.relative_experiment_dir,consts.data.file_name_conventions{II}));
     for JJ = 1:length(dataFiles)
         files.dataFile = dataFiles(JJ).name;
         results(II,JJ) = process_data_point(files,cal,consts,tare(II));
     end
 end
-%
-% %% Create sd
-% % Create sd (statistical data): do N-second averaging here (e.g. 10 second). Split data up into chunks,
-% % do the averaging
-% sd = struct();
-%
-% N = 10; % Length of chuncks in seconds
-% count = 0;
-% fields = fieldnames(td);
-%
-% for i = 1:length(td)
-%     % Determine the number of chunks
-%     start_time = min(td(i).time);
-%     end_time = max(td(i).time);
-%     num_chunks = ceil((end_time - start_time) / N);
-%
-%     % Loop through each chunk
-%     for j = 1:num_chunks
-%         % Find the start and end times for this chunk
-%         chunk_start = start_time + (j-1) * N;
-%         chunk_end = start_time + j * N;
-%
-%         % Find indices of data points within this chunk
-%         indices = find(td(i).time >= chunk_start & td(i).time < chunk_end);
-%
-%         if ~isempty(indices)
-%             count = count + 1;
-%
-%             % Compute the average for each field in the struct
-%             for k = 1:numel(fields)
-%                 if ~strcmp(fields{k}, 'time')
-%                     % Extract the chunk data
-%                     chunk_data = td(i).(fields{k})(indices);
-%                     sd.(fields{k})(count) = mean(chunk_data);
-%                 end
-%
-%             end
-%         end
-%     end
-% end
-%
-% %% Create bd
-% % Create bd (binned data): Bin data by Re and TSR and compute bin average and std
-% bd = struct();
 
+%% Create sd (statistical data)
+N_days = consts.data.N/(24*60*60); % Get sd averaging time in days
+
+if sum(strcmp(consts.data.save_types, 'sd'))
+    % Create sd (statistical data): do N-second averaging here (e.g. 10 second).
+    % Split data up into chunks, do the averaging
+    sd = struct();
+
+    for II = 1:size(results,1)
+        for JJ = 1:size(results,2)
+            count = 0;
+            if ~isempty(results(II,JJ).td)
+                fields = fieldnames(results(II,JJ).td);
+
+                % Determine the number of chunks
+                start_time = min(results(II,JJ).td.time);
+                end_time = max(results(II,JJ).td.time);
+                num_chunks = floor((end_time - start_time) / N_days);
+
+                % Loop through each chunk
+                for KK = 1:num_chunks
+                    % Find the start and end times for this chunk
+                    chunk_start = start_time + (KK-1) * N_days;
+                    chunk_end = start_time + KK * N_days;
+
+                    % Find indices of data points within this chunk
+                    indices = find(results(II,JJ).td.time >= chunk_start & results(II,JJ).td.time < chunk_end);
+
+                    if ~isempty(indices)
+                        count = count + 1;
+
+                        % Compute the average for each field in the struct
+                        for LL = 1:numel(fields)
+                            if ~strcmp(fields{LL}, 'time')
+                                % Extract the chunk data
+                                chunk_data = results(II,JJ).td.(fields{LL})(indices);
+                                results(II,JJ).sd.(fields{LL})(count) = mean(chunk_data);
+                            end
+
+                        end
+                    end
+                end
+            end
+        end
+    end
+end
+
+%% Remove td data if it is not a data type flagged to be saved
+
+if strcmp(consts.data.save_types, 'td') == 0
+    results = rmfield(results,'td');
 end
