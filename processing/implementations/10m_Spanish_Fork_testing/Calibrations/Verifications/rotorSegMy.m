e5a6f1b5ba623739778d3f5190391b850cd056a1
xflow-ian <68661656+xflow-ian@users.noreply.github.com>
2024-09-23 14:30:34 -0700

    Update rotorSegMy.m
    Fixed, moment summation was wrong.

--- processing/implementations/10m_Spanish_Fork_testing/Calibrations/Verifications/rotorSegMy.m
+++ processing/implementations/10m_Spanish_Fork_testing/Calibrations/Verifications/rotorSegMy.m
@@ -2,19 +2,20 @@ clear all
 close all
 clc
 
-
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%NOT WORKING, WHY?%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-
 %% Common inputs
 consts = XFlow_Spanish_Fork_testing_constants();
-verify.func = @(x) - x(1) + x(2);
-verify.data.measurment_channels = {'Lower Arm My','Upper Arm My'};
-verify.data.physical_loads = {'Lower_Arm_My','Upper_Arm_My'};
+verify.func = @(x) x(1)*cosd(consts.lowerArm.angle) +...
+    x(2)*sind(consts.lowerArm.angle) + ...
+    x(3)*cosd(consts.upperArm.angle) + ...
+    x(4)*sind(consts.upperArm.angle);
+verify.data.measurment_channels = {'Lower Arm My','LowerArm Mz',...
+    'Upper Arm My','Upper Arm Mz'};
+verify.data.physical_loads = {'Lower_Arm_My','Lower_Arm_Mz',...
+    'Upper_Arm_My','Upper_Arm_Mz'};
 verify.data.absolute_cali_path = 'C:\Users\Ian\Documents\GitHub\xflow-daq\processing\implementations\10m_Spanish_Fork_testing\Calibrations\Results\';
 verify.data.relative_cali_struct = {'lower_arm_cal_single_axis_struct.mat',...
+    'lower_arm_cal_single_axis_struct.mat',...
+    'upper_arm_cal_single_axis_struct.mat',...
     'upper_arm_cal_single_axis_struct.mat'};
 
 %% Rotor segment on ground
@@ -29,3 +30,29 @@ verify.applied_load_scaling = consts.units.lbf_to_N*consts.upperArm.span*cosd(co
 figure
 plot(applied_load,measured_load,'o')
 hold on
+
+%% Raised rotor
+verify.absolute_data_path = 'X:\Experiments and Data\20 kW Prototype\Loads_Data\load_calibrations\installed_rotor';
+verify.tdms_filter = '*rotor_strain*.tdms';
+verify.applied_load_var_name = 'appliedLoad';
+verify.data.measurment_channels = {'Lower Arm My','Lower Arm Mz',...
+    'Upper Arm My','Upper Arm Mz'};
+data_folders = {'-X','+X'};
+applied_load_scaling = consts.units.lbf_to_N*[1 -1]*consts.upperArm.span*cosd(consts.upperArm.angle);
+
+for II = 1:length(data_folders)
+    verify.relative_data_folder = data_folders{II};
+    verify.applied_load_scaling = applied_load_scaling(II);
+
+    [applied_load, measured_load] = calibration_verification(verify);
+
+    plot(applied_load,measured_load,'o')
+    hold on
+end
+
+%% Cleanup figure
+x = [-3000 4000];
+plot(x,x,'--k')
+legend('Rotor Segment on Ground','Rotor Raised, -X', 'Rotor Raised, +X','Location','SouthEast')
+xlabel('Applied Load')
+ylabel('Measured Load')
