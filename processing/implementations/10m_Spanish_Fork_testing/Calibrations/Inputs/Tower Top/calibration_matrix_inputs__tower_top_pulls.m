6887349774a32e270f3a62e9896e2d17c3927a1c
xflow-ian <68661656+xflow-ian@users.noreply.github.com>
2024-09-30 10:58:16 -0700

    Added VA-HA case

--- processing/implementations/10m_Spanish_Fork_testing/Calibrations/Inputs/Tower Top/calibration_matrix_inputs__tower_top_pulls.m
+++ processing/implementations/10m_Spanish_Fork_testing/Calibrations/Inputs/Tower Top/calibration_matrix_inputs__tower_top_pulls.m
@@ -24,11 +24,19 @@ crosstalk.channel_names = {'Upper GW N','Upper GW S','Upper GW E','Upper GW W',.
 crosstalk.output_units = {'N','N','N-m','N-m'};
 calibNum = 0; % Initiate counting varible
 
-% %% V-A, H-A
-% calibNum = calibNum + 1;
-% calib(calibNum).folder = 'V-A_H-A';
-% dist(calibNum) = [];
-% % calib(calibNum).applied_load_scaling = -consts.units.lbf_to_N*[a; a*(L/2+d)/L; 0];
+%% V-A, H-A
+calibNum = calibNum + 1;
+calib(calibNum).folder = 'V-A_H-A';
+
+% % Manually recorded information
+angle =[0.8; 0.8];
+manual_applied_loads = [0, 0; 2116, 2111; 1132, 1146; 0, 0];
+coordinate = S_GW__NE_bolt + [-2*12, 5]*consts.units.inch_to_m;
+
+% Calculated inputs
+theta = repmat(atand(coordinate(2)./ coordinate(1)),2,1);
+calib(calibNum).applied_load_scaling = cosd(angle).*[sind(theta), cosd(theta), sind(theta)*tower_top_height, cosd(theta)*tower_top_height];
+calib(calibNum).corrections.load = [(1:length(manual_applied_loads))', mean(manual_applied_loads,2)];
 
 %% V-A, H-B
 calibNum = calibNum + 1;
@@ -38,7 +46,7 @@ calib(calibNum).folder = 'V-A_H-B';
 E_GW_dist = [39*12 + 11, 40*12 + 1, 40*12 + 3, 40*12 + 4]*consts.units.inch_to_m;
 S_GW_dist = [16*12 + 6, 16*12 + 5.5, 16*12 + 7, 16*12 + 7]*consts.units.inch_to_m;
 W_GW_dist = [16*12 + 9, 16*12 + 7 16*12 + 6, 16*12 + 6.5]*consts.units.inch_to_m;
-angle = [0.5, 0.3,0.30, 0.50];
+angle = [0.5; 0.3; 0.30; 0.50];
 manual_applied_loads = [0, 0; 1957, 1951; 1177.5, 1182.5; 0, 0; 1171.2, 1179; 1885, 1882.5; 0, 0];
 
 % Calculated inputs
@@ -55,7 +63,7 @@ calib(calibNum).folder = 'V-A_H-C';
 E_GW_dist = [18*12 + 1, 18*12 + 7, 18*12 + 3, 18*12 + 9]*consts.units.inch_to_m;
 S_GW_dist =[15*12 + 7, 15*12 + 1, 15*12 + 7, 15*12 + 6]*consts.units.inch_to_m;
 W_GW_dist = [39*12 + 3, 39*12 + 2, 39*12 + 1, 39*12 + 1]*consts.units.inch_to_m;
-angle = [2.8, 2.8, 2.1, 3];
+angle = [2.8; 2.8; 2.1; 3];
 manual_applied_loads = [0, 0; 0, 0; 1976.5, 1975.5; 965.5, 969.5; 0, 0; 0, 0; 880.5, 880.5; 2049, 2050; 0, 0];
 
 % Calculated inputs
@@ -69,10 +77,10 @@ calib(calibNum).folder = 'V-B_H-B';
 
 % Manually recorded information
 E_GW_dist = [42*12 + 4, 42*12 + 5, 42*12 + 11, 43*12]*consts.units.inch_to_m;
-S_GW_dist =[17*12 + 2, 17*12 + 2.5, 17*12 + 3.5, 17*12 + 7 ]*consts.units.inch_to_m;
-W_GW_dist = [16*12 + 11, 16*12 + 11.5, 16*12 + 10.5, 16*12 + 11 ]*consts.units.inch_to_m;
-angle = [(-9.3+-10.6)/2, (-10.1+-11.5)/2,...
-    (-10.4+-11.0)/2, (-9.1+-9.1)/2];
+S_GW_dist =[17*12 + 2, 17*12 + 2.5, 17*12 + 3.5, 17*12 + 7]*consts.units.inch_to_m;
+W_GW_dist = [16*12 + 11, 16*12 + 11.5, 16*12 + 10.5, 16*12 + 11]*consts.units.inch_to_m;
+angle = [(-9.3+-10.6)/2; (-10.1+-11.5)/2;...
+    (-10.4+-11.0)/2; (-9.1+-9.1)/2];
 manual_applied_loads = [0, 0; 1804, 1804; 924, 923; 0, 0; 1133, 1136; 1969, 1964; 0, 0];
 
 % Calculated inputs
@@ -85,10 +93,10 @@ calibNum = calibNum + 1;
 calib(calibNum).folder = 'V-B_H-C';
 
 % Manually recorded information
-E_GW_dist = [18*12 + 0, 18*12 + 1, 18*12 + 0, 17*12 + 11 ]*consts.units.inch_to_m;
-S_GW_dist =[15*12 + 6.5, 15*12 + 4, 15*12 + 6, 15*12 + 6 ]*consts.units.inch_to_m;
-W_GW_dist = [37*12 + 10, 37*12 + 5, 37*12 + 8, 37*12 + 7 ]*consts.units.inch_to_m;
-angle =[-11.9, -12.3, -11.8, -11.9];
+E_GW_dist = [18*12 + 0, 18*12 + 1, 18*12 + 0, 17*12 + 11]*consts.units.inch_to_m;
+S_GW_dist =[15*12 + 6.5, 15*12 + 4, 15*12 + 6, 15*12 + 6]*consts.units.inch_to_m;
+W_GW_dist = [37*12 + 10, 37*12 + 5, 37*12 + 8, 37*12 + 7]*consts.units.inch_to_m;
+angle =[-11.9; -12.3; -11.8; -11.9];
 manual_applied_loads = [0, 0; 1982, 1979; 815, 808.5; 0, 0; 975, 976; 2043.5, 2045; 0, 0];
 
 % Calculated inputs
@@ -97,13 +105,4 @@ calib(calibNum).applied_load_scaling = cosd(angle).*[sind(theta), cosd(theta), s
 calib(calibNum).corrections.load = [(1:length(manual_applied_loads))', mean(manual_applied_loads,2)];
 
 
-%% V-A, H-A
-% calibNum = calibNum + 1;
-% calib(calibNum).folder = 'V-A_H-A';
 
-% % Manually recorded information
-% angle =[0.8, 0.8];
-% manual_applied_loads = [0, 0; 2116, 2111; 1132, 1146; 0, 0];
-% 
-% % Calculated inputs
-% calib(calibNum).corrections.load = [(1:length(manual_applied_loads))', mean(manual_applied_loads,2)];
