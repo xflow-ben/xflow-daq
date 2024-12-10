aec8a816fcee4c9236988eb5624a282483620c3c
xflow-ben <110842449+xflow-ben@users.noreply.github.com>
2024-12-10 10:12:00 -0800

    Update calculate_sd.m

--- processing/utilities/Data Analysis/calculate_sd.m
+++ processing/utilities/Data Analysis/calculate_sd.m
@@ -1,4 +1,4 @@
-function results = calculate_sd(results,consts)
+function sd = calculate_sd(td,consts)
 
 %% Create sd (statistical data)
 N = consts.data.N; % Get sd averaging time [s]
@@ -6,52 +6,52 @@ N = consts.data.N; % Get sd averaging time [s]
 if sum(strcmp(consts.data.save_types, 'sd'))
     % Create sd (statistical data): do N-second averaging here (e.g. 10 second).
     % Split data up into chunks, do the averaging
-    results.sd = struct();
+    sd = struct();
 
     count = 0;
-    fields = fieldnames(results.td);
+    fields = fieldnames(td);
 
     % Determine the number of chunks
-    start_time = min(results.td.Time);
-    end_time = max(results.td.Time);
+    start_time = min(td.Time);
+    end_time = max(td.Time);
 
 
     if isnan(consts.data.N)% no time span specified, so average the entire file
-        N = end_time-start_time;
+        N = seconds(end_time-start_time);
         num_chunks = 1;
     else
-        num_chunks = ceil((end_time - start_time) / N);
+        num_chunks = ceil(seconds(end_time - start_time) / N);
     end
 
 
     % Loop through each chunk
     for KK = 1:num_chunks
         % Find the start and end times for this chunk
-        chunk_start = start_time + (KK-1) * N;
-        chunk_end = start_time + KK * N;
+        chunk_start = start_time + seconds((KK-1) * N);
+        chunk_end = start_time + seconds(KK * N);
 
         % Find indices of data points within this chunk
-        indices = find(results.td.Time >= chunk_start & results.td.Time < chunk_end);
+        indices = find(td.Time >= chunk_start & td.Time < chunk_end);
 
         if ~isempty(indices)
-            % if sum(isnan(results.td.(fields{1})(indices))) == 0 % Don't except chunks with nans
+            % if sum(isnan(td.(fields{1})(indices))) == 0 % Don't except chunks with nans
                 count = count + 1;
 
                 % Compute the average for each field in the struct
                 for LL = 1:numel(fields)
                     if ~strcmp(fields{LL}, 'Time')
                     % Extract the chunk data statistics
-                    chunk_data = results.td.(fields{LL})(indices);
+                    chunk_data = td.(fields{LL})(indices);
 
-                    results.sd.(fields{LL}).mean(count) = mean(chunk_data,1,'omitnan');
+                    sd.(fields{LL}).mean(count) = mean(chunk_data,1,'omitnan');
 
-                    results.sd.(fields{LL}).std(count) = std(chunk_data,'omitnan');
-                    results.sd.(fields{LL}).min(count) = min(chunk_data,[],'omitnan');
-                    results.sd.(fields{LL}).max(count) = max(chunk_data,[],'omitnan');
+                    sd.(fields{LL}).std(count) = std(chunk_data,'omitnan');
+                    sd.(fields{LL}).min(count) = min(chunk_data,[],'omitnan');
+                    sd.(fields{LL}).max(count) = max(chunk_data,[],'omitnan');
                     end
                 end
-                results.sd.td_index.start(count) = indices(1);
-                results.sd.td_index.end(count) = indices(end);
+                sd.td_index.start(count) = indices(1);
+                sd.td_index.end(count) = indices(end);
             % end
         end
     end
